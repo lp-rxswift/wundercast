e2fb83616bd371660e94bba40fad7756fd77629c
Lucas <lucaspedrazoli@gmail.com>
2020-12-10 16:00:34 -0300

    add extensions files

---
+++ Wundercast/Controllers/Extensions/CLLocationManager+Rx.swift
@@ -0,0 +1,12 @@
+//
+//  CLLocationManager+Rx.swift
+//  Wundercast
+//
+//  Created by Lucas Pedrazoli on 10/12/20.
+//  Copyright © 2020 Ray Wenderlich. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+import RxSwift
+import RxCocoa
---
+++ Wundercast/Controllers/Extensions/MKMapView+Rx.swift
@@ -0,0 +1,12 @@
+//
+//  MKMapView+Rx.swift
+//  Wundercast
+//
+//  Created by Lucas Pedrazoli on 10/12/20.
+//  Copyright © 2020 Ray Wenderlich. All rights reserved.
+//
+
+import Foundation
+import MapKit
+import RxSwift
+import RxCocoa
