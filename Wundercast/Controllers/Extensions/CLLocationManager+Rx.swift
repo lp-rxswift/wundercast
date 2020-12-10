8c3687d023df622d0435aa2a9686a994ba58ad79
Lucas <lucaspedrazoli@gmail.com>
2020-12-10 16:34:19 -0300

    extend CLLocationManager to rx with low level rx api code D:

--- Wundercast/Controllers/Extensions/CLLocationManager+Rx.swift
+++ Wundercast/Controllers/Extensions/CLLocationManager+Rx.swift
@@ -10,3 +10,37 @@ import Foundation
 import CoreLocation
 import RxSwift
 import RxCocoa
+
+extension CLLocationManager: HasDelegate {}
+
+class RxCLLocationManagerDelegateProxy:
+  DelegateProxy<CLLocationManager, CLLocationManagerDelegate>,
+  DelegateProxyType,
+  CLLocationManagerDelegate {
+
+  weak public private(set) var locationManager: CLLocationManager?
+
+  public init(locationManager: ParentObject) {
+    self.locationManager = locationManager
+    super.init(parentObject: locationManager,
+               delegateProxy: RxCLLocationManagerDelegateProxy.self)
+  }
+
+  static func registerKnownImplementations() {
+    register { RxCLLocationManagerDelegateProxy(locationManager: $0) }
+  }
+}
+
+public extension Reactive where Base: CLLocationManager {
+  var delegate: DelegateProxy<CLLocationManager, CLLocationManagerDelegate> {
+    RxCLLocationManagerDelegateProxy.proxy(for: base)
+  }
+
+  var didUpdateLocations: Observable<[CLLocation]> {
+    delegate.methodInvoked(
+      #selector(CLLocationManagerDelegate.locationManager(_:didUpdateLocations:)))
+      .map { parameters in
+        parameters[1] as! [CLLocation]
+      }
+  }
+}
